7a37333481213b29a3e696c60301de0e6517b5b9
MaxMaxiee <albamaxine99@gmail.com>
2024-03-01 16:51:33 +0800

    S12V9

--- app/core/models.py
+++ app/core/models.py
@@ -8,15 +8,15 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
-
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
         """Creat, save and return a new user."""
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+
         if not email:
             raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
