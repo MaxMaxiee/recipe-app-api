f578e5718b6fc91242f6b2751b4dc8ca5de48124
MaxMaxiee <albamaxine99@gmail.com>
2024-03-04 14:49:18 +0800

    Ready for Deployment

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -1,6 +1,7 @@
 """
 Test for models
 """
+
 from unittest.mock import patch
 from decimal import Decimal
 
@@ -9,17 +10,19 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
-def create_user(email='user@example.com', password='testpass123'):
+
+def create_user(email="user@example.com", password="testpass123"):
     """Create and return a new user."""
     return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
-        email = 'test@example.com'
-        password = 'testpass123'
+        email = "test@example.com"
+        password = "testpass123"
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
@@ -44,15 +47,13 @@ class ModelTests(TestCase):
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'test123')
+            get_user_model().objects.create_user("", "test123")
 
     def test_create_superuser(self):
         """Test creating a superuser."""
 
         user = get_user_model().objects.create_superuser(
-            'test@example.com',
-            'test123'
-            )
+            "test@example.com", "test123")
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
@@ -60,15 +61,15 @@ class ModelTests(TestCase):
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
         user = get_user_model().objects.create_user(
-            'test@example.com',
-            'testpass123',
+            "test@example.com",
+            "testpass123",
         )
         recipe = models.Recipe.objects.create(
             user=user,
-            title='Sample recipe name',
+            title="Sample recipe name",
             time_minutes=5,
-            price=Decimal('5.50'),
-            description='Sample recipe description.',
+            price=Decimal("5.50"),
+            description="Sample recipe description.",
         )
 
         self.assertEqual(str(recipe), recipe.title)
@@ -76,7 +77,7 @@ class ModelTests(TestCase):
     def test_create_tag(self):
         """Test creating a tag is successful."""
         user = create_user()
-        tag = models.Tag.objects.create(user=user, name='Tag1')
+        tag = models.Tag.objects.create(user=user, name="Tag1")
 
         self.assertEqual(str(tag), tag.name)
 
@@ -84,17 +85,15 @@ class ModelTests(TestCase):
         """Test ceating an ingredient is successful."""
         user = create_user()
         ingredient = models.Ingredient.objects.create(
-            user=user,
-            name='Ingredient1'
-        )
+            user=user, name="Ingredient1")
 
         self.assertEqual(str(ingredient), ingredient.name)
 
-    @patch('core.models.uuid.uuid4')
+    @patch("core.models.uuid.uuid4")
     def test_recipe_file_name_uuid(self, mock_uuid):
         """Test generating image path."""
-        uuid = 'test-uuid'
+        uuid = "test-uuid"
         mock_uuid.return_value = uuid
-        file_path = models.recipe_image_file_path(None, 'example.jpg')
+        file_path = models.recipe_image_file_path(None, "example.jpg")
 
-        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
+        self.assertEqual(file_path, f"uploads/recipe/{uuid}.jpg")
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,7 @@
 """
 Views for the recipe APIs.
 """
+
 from drf_spectacular.utils import (
     extend_schema_view,
     extend_schema,
@@ -24,24 +25,26 @@ from core.models import (
 )
 from recipe import serializers
 
+
 @extend_schema_view(
     list=extend_schema(
         parameters=[
             OpenApiParameter(
-                'tags',
+                "tags",
                 OpenApiTypes.STR,
-                description='Comma separated list of tag IDs to filter',
+                description="Comma separated list of tag IDs to filter",
             ),
             OpenApiParameter(
-                'ingredients',
+                "ingredients",
                 OpenApiTypes.STR,
-                description='Comma separated list of ingredient IDs to filter',
+                description="Comma separated list of ingredient IDs to filter",
             ),
         ]
     )
 )
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
+
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -49,12 +52,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def _params_to_ints(self, qs):
         """Convert a list of strings to integers."""
-        return [int(str_id) for str_id in qs.split(',')]
+        return [int(str_id) for str_id in qs.split(",")]
 
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        tags = self.request.query_params.get('tags')
-        ingredients = self.request.query_params.get('ingredients')
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get("ingredients")
         queryset = self.queryset
         if tags:
             tag_ids = self._params_to_ints(tags)
@@ -63,15 +66,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
             ingredient_ids = self._params_to_ints(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredient_ids)
 
-        return queryset.filter(
-            user=self.request.user
-        ).order_by('-id').distinct()
+        return queryset.filter(user=self.request.user
+                               ).order_by("-id").distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
-        if self.action == 'list':
+        if self.action == "list":
             return serializers.RecipeSerializer
-        elif self.action == 'upload_image':
+        elif self.action == "upload_image":
             return serializers.RecipeImageSerializer
 
         return self.serializer_class
@@ -80,7 +82,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
-    @action(methods=['POST'], detail=True, url_path='upload-image')
+    @action(methods=["POST"], detail=True, url_path="upload-image")
     def upload_image(self, request, pk=None):
         """Upload an image to recipe."""
         recipe = self.get_object()
@@ -93,49 +95,50 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 @extend_schema_view(
     list=extend_schema(
         parameters=[
             OpenApiParameter(
-                'assigned_only',
-                OpenApiTypes.INT, enum=[0, 1],
-                description='Filter by items assigned to recipes.',
+                "assigned_only",
+                OpenApiTypes.INT,
+                enum=[0, 1],
+                description="Filter by items assigned to recipes.",
             )
         ]
     )
 )
-class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
-                            mixins.UpdateModelMixin,
-                            mixins.ListModelMixin,
-                            viewsets.GenericViewSet):
-    """"Base viewset for recipe attributes."""
+class BaseRecipeAttrViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """ "Base viewset for recipe attributes."""
+
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Filter querset to authenticated user."""
-        assigned_only = bool(
-            int(self.request.query_params.get('assigned_only', 0))
-        )
+        assigned_only = bool(int(self.request.query_params.get(
+            "assigned_only", 0)))
         queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
 
-        return queryset.filter(
-            user=self.request.user
-        ).order_by('-name').distinct()
+        return queryset.filter(user=self.request.user
+                               ).order_by("-name").distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
     """Manage tags in the database."""
+
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
 
 
 class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingridents in the database"""
+
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-
-
